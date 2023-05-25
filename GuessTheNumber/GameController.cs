1e4a39a918530e30a1bfbe3d51fec45ce782fc30
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-25 18:21:45 +0100

    started construction of main loop

--- GuessTheNumber/GameController.cs
+++ GuessTheNumber/GameController.cs
@@ -9,11 +9,27 @@ namespace GuessTheNumber
         //Game view
         private readonly GameView gameView;
 
-        //will be used for main and build the project
+        /// <summary>
+        /// will be used for main and build the project
+        /// </summary>
         public GameController()
         {
             gameModel = new GameModel();
             gameView = new GameView();
         }
+
+        /// <summary>
+        /// Main game loop
+        /// </summary>
+        public void StartGame()
+        {
+            //We'll be used in another class to display beginning portion
+            gameView.DisplayWelcomeMessage();
+
+            //loop
+            while (!gameModel.GuessedCorrectly)
+            {
+
+            }
     }
 }
