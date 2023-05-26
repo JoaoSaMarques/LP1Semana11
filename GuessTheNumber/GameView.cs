db9de709e5d403025f43a8d646774cd4e56c8091
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-26 09:43:26 +0100

    Game View/Messages

---
+++ GuessTheNumber/GameView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    /// <summary>
+    /// This will be used to construct the messages written for the player
+    /// </summary>
+    public class GameView
+    {
+
+    }
+}
