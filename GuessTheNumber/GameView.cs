341d301d50370d4d298ee7e59bfa44ad4dd08c6b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-26 09:44:11 +0100

    Intro

--- GuessTheNumber/GameView.cs
+++ GuessTheNumber/GameView.cs
@@ -7,6 +7,13 @@ namespace GuessTheNumber
     /// </summary>
     public class GameView
     {
-
+        /// <summary>
+        /// Start message/Instructions at the start
+        /// </summary>
+        public void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Guess the Number!");
+            Console.WriteLine("I have chosen a number between 1 and 100.");
+        }
     }
 }
