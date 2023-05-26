582b5c5ff924c1f3a494d28680a05f51032aee4b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-26 09:39:08 +0100

    get and set values

--- GuessTheNumber/GameModel.cs
+++ GuessTheNumber/GameModel.cs
@@ -2,9 +2,17 @@
 
 namespace GuessTheNumber
 {
+    /// <summary>
+    /// Model/constructor for other code
+    /// </summary>
     public class GameModel
     {
-
+        //Number player has to reach
+        public int TargetNumber { get; }
+        //Number of attempts made by the player
+        public int Attempts { get; set; }
+        //Correct guess
+        public bool GuessedCorrectly { get; set; }
     }
     
 }
