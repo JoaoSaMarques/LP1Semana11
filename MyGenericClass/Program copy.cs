d4c3fedc92c15a81a6b7d5ac62135155939eab3b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-22 11:38:12 +0100

    Create guarda3

---
+++ MyGenericClass/Program copy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T _item1;
+        private T _item2;
+        private T _item3;
+
+        public Guarda3()
+        {
+            _item1 = default(T);
+            _item2 = default(T);
+            _item3 = default(T);
+        }
+
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return i == 0? _item1 : i == 1? _item2 : _item3;
+        }
+
+        
+    }
+}
