660a2eaa3834884dd8d8b5fb92c9b1795c1b3327
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-22 11:38:48 +0100

    update main

--- MyGenericClass/Program.cs
+++ MyGenericClass/Program.cs
@@ -6,7 +6,21 @@ namespace MyGenericClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Guarda3<string> stringGuard = new Guarda3<string>();
+            stringGuard.SetItem(0, "Hello");
+            stringGuard.SetItem(1, "World");
+            stringGuard.SetItem(2, "!");
+            Console.WriteLine(stringGuard.GetItem(0));
+            Console.WriteLine(stringGuard.GetItem(1));
+            Console.WriteLine(stringGuard.GetItem(2));
+
+            Guarda3<float> floatGuard = new Guarda3<float>();
+            floatGuard.SetItem(0, 3.14f);
+            floatGuard.SetItem(1, 2.718f);
+            floatGuard.SetItem(2, 1.618f);
+            Console.WriteLine(floatGuard.GetItem(0));
+            Console.WriteLine(floatGuard.GetItem(1));
+            Console.WriteLine(floatGuard.GetItem(2));
         }
     }
 }
