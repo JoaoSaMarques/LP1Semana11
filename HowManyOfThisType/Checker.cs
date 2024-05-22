35f10ce252c4c334c582ef9977566096bfd0061c
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-22 11:19:19 +0100

    create Checker and added collection to main

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            return items.Count(item => item is T);
+        }
+    }
+}
---
+++ HowManyOfThisType/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowManyOfThisType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> objects = new List<object> {1, "test", 2, 3.5, 4};
+            int count = Checker.HowManyOfType<int>(objects);
+            Console.WriteLine($"Number of integers: {count}");
+        }
+    }
+}
