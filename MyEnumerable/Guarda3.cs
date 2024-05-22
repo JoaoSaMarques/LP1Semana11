6e287a37073b6349c56e3918c2093d76d7de576d
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-05-22 11:48:18 +0100

    update to have IEnumerable and system.collections

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T _item1;
+        private T _item2;
+        private T _item3;
+
+        public Guarda3()
+        {
+            _item1 = default(T);
+            _item2 = default(T);
+            _item3 = default(T);
+        }
+
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return i == 0? _item1 : i == 1? _item2 : _item3;
+        }
+
+        public void SetItem(int i, T item)
+        {
+            if (i < 0 || i > 2)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (i == 0)
+            {
+                _item1 = item;
+            }
+            else if (i == 1)
+            {
+                _item2 = item;
+            }
+            else
+            {
+                _item3 = item;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return _item1;
+            yield return _item2;
+            yield return _item3;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- MyEnumerable/Program.cs
+++ MyEnumerable/Program.cs
@@ -4,9 +4,27 @@ namespace MyEnumerable
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Guarda3<string> stringGuard = new Guarda3<string>();
+            stringGuard.SetItem(0, "Hello");
+            stringGuard.SetItem(1, "World");
+            stringGuard.SetItem(2, "!");
+
+            foreach (var item in stringGuard)
+            {
+                Console.WriteLine(item);
+            }
+
+            Guarda3<float> floatGuard = new Guarda3<float>();
+            floatGuard.SetItem(0, 3.14f);
+            floatGuard.SetItem(1, 2.718f);
+            floatGuard.SetItem(2, 1.618f);
+
+            foreach (var item in floatGuard)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
