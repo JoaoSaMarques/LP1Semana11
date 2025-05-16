b7e0616917716a8a447b6e530d2208ebb2bdbbe2
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-16 13:54:27 +0100

    update main

--- MathDuel/Program.cs
+++ MathDuel/Program.cs
@@ -2,59 +2,35 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Random rand = new Random();
-        int score = 0;
-        int wrongAnswers = 0;
-
-        Console.WriteLine("=== Math Duel ===");
-        Console.WriteLine("Answer the math problems correctly!");
-        Console.WriteLine("You can make up to 3 mistakes.");
-        Console.WriteLine();
-
-        while (wrongAnswers < 3)
+        string problemFile = null;
+        if (args.Length > 0)
         {
-            int a = rand.Next(1, 11);     // 1 to 10
-            int b = rand.Next(1, 11);
-            int operation = rand.Next(3); // 0: +, 1: -, 2: *
-
-            int correctAnswer;
-            string question;
-
-            if (operation == 0)
-            {
-                correctAnswer = a + b;
-                question = $"{a} + {b} = ?";
-            }
-            else if (operation == 1)
-            {
-                correctAnswer = a - b;
-                question = $"{a} - {b} = ?";
-            }
-            else
-            {
-                correctAnswer = a * b;
-                question = $"{a} * {b} = ?";
-            }
-
-            Console.Write("Question: " + question + " ");
-            string input = Console.ReadLine();
-
-            int playerAnswer = int.Parse(input);
-
-            if (playerAnswer == correctAnswer)
-            {
-                Console.WriteLine("Correct!\n");
-                score++;
-            }
-            else
-            {
-                Console.WriteLine($"Wrong! The correct answer was {correctAnswer}.\n");
-                wrongAnswers++;
-            }
+            problemFile = args[0];
         }
 
-        Console.WriteLine($"Game over! Your final score is: {score}");
+        MathDuel.IView view = new MathDuel.ConsoleView();
+        System.Collections.Generic.IEnumerable<MathDuel.MathProblem> problems;
+
+        if (!string.IsNullOrEmpty(problemFile))
+        {
+            try
+            {
+                problems = MathDuel.GameController.ReadProblemsFromFile(problemFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading problem file: {ex.Message}");
+                return;
+            }
+        }
+        else
+        {
+            problems = MathDuel.GameController.GenerateRandomProblems();
+        }
+
+        var controller = new MathDuel.GameController(view, problems);
+        controller.Run();
     }
 }
