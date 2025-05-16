e334df535b0c3d6fc8676f54a91a1df0fb1498ed
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-16 13:21:06 +0100

    create PlayerManagerMVC2

---
+++ PlayerManagerMVC2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManagerMVC2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
